ae450f7ff9a55c9f1a0f07cc07ed8b012714edf4
Mislav <mislavjc@gmail.com>
2024-02-13 01:11:37 +0700

    feat: build the prompt for paths and save to generated dir

--- src/commands/index.tsx
+++ src/commands/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Spinner, TextInput } from '@inkjs/ui';
 import { exec } from 'child_process';
+import { writeFile } from 'fs/promises';
 import { Text } from 'ink';
 import yaml from 'js-yaml';
 import OpenAI from 'openai';
 
 import { readEnvVars, updateEnvVars } from '../lib/env.js';
+import { buildPrompt } from '../lib/prompt.js';
 
 const openai = new OpenAI({
   apiKey: readEnvVars().OPENAI_API_KEY,
@@ -70,75 +72,72 @@ export default function Index() {
     const doc = yaml.load(yamlContent);
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    const firstProp = Object.keys(
+    const paths = Object.keys(
       (
         doc as {
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
           paths: unknown;
         }
       ).paths as object
-    )[2];
+    ).slice(0, 10);
 
-    // @ts-expect-error no types
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
-    const firstPropValue = doc.paths[firstProp];
+    const promises = paths.map(async (path) => {
+      // @ts-expect-error no types
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      const firstPropValue = doc.paths[path];
+      const prompt = buildPrompt(JSON.stringify(firstPropValue));
 
-    const prompt = `Return the JSON for a function calling object for the object, 
-                    <EXAMPLE OBJECT>
-                      {
-                        operationId: 'get-an-album',
-                        'x-spotify-policy-list': { '$ref': '#/components/x-spotify-policy/metadataPolicyList' },
-                        tags: [ 'Albums' ],
-                        summary: 'Get Album\n',
-                        description: 'Get Spotify catalog information for a single album.\n',
-                        parameters: [
-                          { '$ref': '#/components/parameters/PathAlbumId' },
-                          { '$ref': '#/components/parameters/QueryMarket' }
-                        ],
-                        responses: {
-                          '200': { '$ref': '#/components/responses/OneAlbum' },
-                          '401': { '$ref': '#/components/responses/Unauthorized' },
-                          '403': { '$ref': '#/components/responses/Forbidden' },
-                          '429': { '$ref': '#/components/responses/TooManyRequests' }
-                        },
-                        security: [ { oauth_2_0: [] } ]
-                      }
-                    <EXAMPLE RETURN>
-                        {
-                          name: 'get_an_album',
-                          description: 'Get Spotify catalog information for a single album.',
-                          parameters: [
-                            { name: 'album_id', type: 'string' },
-                            { name: 'market', type: 'string' }
-                          ],
-                        },
-                    </EXAMPLE RETURN>
+      const completion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content: 'You are a friendly AI assistant that only returns JSON',
+          },
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+      });
 
-                    <OBJECT>
-                      ${JSON.stringify(firstPropValue)}
-                    </OBJECT>
-
-                    Now create return JSON for the function calling object for the object above.
-
-                    Do not include the <EXAMPLE OBJECT> or <EXAMPLE RETURN> tags in your response.
-                `;
-
-    const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
-          content: 'You are a friendly AI assistant that only returns JSON',
-        },
-        {
-          role: 'user',
-          content: prompt,
-        },
-      ],
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return JSON.parse(completion.choices[0]?.message.content || '');
     });
 
-    // @ts-expect-error no types
-    console.log(completion.choices[0].message.content);
+    try {
+      const functionCalls = await Promise.all(promises);
+      console.log(functionCalls);
+
+      const functionCallsString = JSON.stringify(functionCalls, null, 2);
+
+      const content = `export const functions = ${functionCallsString};`;
+
+      await writeFile('./generated/functions.ts', content, 'utf8');
+
+      console.log(
+        'File generated/functions.ts has been created with the function calls.'
+      );
+
+      exec(
+        'npx eslint ./generated/functions.ts --fix',
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error running ESLint: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            console.error(`ESLint stderr: ${stderr}`);
+            return;
+          }
+
+          console.log('ESLint has been run on generated/functions.ts');
+          console.log(stdout);
+        }
+      );
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
   };
 
   return (
---
+++ src/lib/prompt.ts
@@ -0,0 +1,41 @@
+export const buildPrompt = (object: string) => {
+  return `Return the JSON for a function calling object for the object, 
+            <EXAMPLE OBJECT>
+              {
+                operationId: 'get-an-album',
+                'x-spotify-policy-list': { '$ref': '#/components/x-spotify-policy/metadataPolicyList' },
+                tags: [ 'Albums' ],
+                summary: 'Get Album\n',
+                description: 'Get Spotify catalog information for a single album.\n',
+                parameters: [
+                  { '$ref': '#/components/parameters/PathAlbumId' },
+                  { '$ref': '#/components/parameters/QueryMarket' }
+                ],
+                responses: {
+                  '200': { '$ref': '#/components/responses/OneAlbum' },
+                  '401': { '$ref': '#/components/responses/Unauthorized' },
+                  '403': { '$ref': '#/components/responses/Forbidden' },
+                  '429': { '$ref': '#/components/responses/TooManyRequests' }
+                },
+                security: [ { oauth_2_0: [] } ]
+              }
+            <EXAMPLE RETURN>
+                {
+                  name: 'get_an_album',
+                  description: 'Get Spotify catalog information for a single album.',
+                  parameters: [
+                    { name: 'album_id', type: 'string' },
+                    { name: 'market', type: 'string' }
+                  ],
+                  ,
+            </EXAMPLE RETURN>
+
+            <OBJECT>
+              ${object}
+            </OBJECT>
+
+            Now create return JSON for the function calling object for the object above.
+
+            Do not include the <EXAMPLE OBJECT> or <EXAMPLE RETURN> tags in your response.
+        `;
+};
