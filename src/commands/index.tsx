fe40a225b7c42c07eac81f34106db56057b367b5
Mislav <mislavjc@gmail.com>
2024-02-18 14:00:59 +0700

    refactor: extract logic from main file

--- src/commands/index.tsx
+++ src/commands/index.tsx
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Spinner, TextInput } from '@inkjs/ui';
-import { exec } from 'child_process';
-import { writeFile } from 'fs/promises';
 import { Text } from 'ink';
 import yaml from 'js-yaml';
-import OpenAI from 'openai';
 
 import { OpenAPIGenericSchema } from '../../types/openapi.js';
-import { generateRunFunction } from '../lib/ai.js';
 import { readEnvVars, updateEnvVars } from '../lib/env.js';
 import {
+  generateFunctionCalls,
+  generateSchema,
+  getOperations,
   resolveComponentReferences,
   stripResponsesFromSchema,
 } from '../lib/openapi.js';
-import { buildPrompt } from '../lib/prompt.js';
-
-const openai = new OpenAI({
-  apiKey: readEnvVars().OPENAI_API_KEY,
-});
 
 export default function Index() {
   const [step, setStep] = useState(1);
@@ -56,26 +50,9 @@ export default function Index() {
 
     const yamlContent = await fetch(newUrl).then((res) => res.text());
 
-    const command = `npx openapi-typescript ${newUrl} --o ./generated/api.ts`;
+    generateSchema(newUrl);
 
-    exec(command, (error, _stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing command: ${error.message}`);
-        // Handle error here
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        // Handle stderr here
-        return;
-      }
-
-      console.log(
-        "File generated/api.ts has been created with the OpenAPI schema's types."
-      );
-
-      setStep(4);
-    });
+    setStep(4);
 
     const doc = stripResponsesFromSchema(
       resolveComponentReferences(yaml.load(yamlContent) as OpenAPIGenericSchema)
@@ -83,86 +60,13 @@ export default function Index() {
 
     const paths = Object.keys(doc.paths).slice(0, 10);
 
-    const operations = paths.flatMap((path) => {
-      const pathValue = doc.paths[path];
+    const operations = getOperations({ paths, doc });
 
-      if (!pathValue) {
-        throw new Error(`No value found for path ${path}`);
-      }
-
-      const methods = Object.keys(pathValue);
-
-      return methods.map((method) => {
-        const operation = pathValue[method];
-
-        if (!operation) {
-          throw new Error(`No value found for method ${method}`);
-        }
-
-        return {
-          path,
-          operationId: operation.operationId as string,
-          method,
-        };
-      });
+    await generateFunctionCalls({
+      paths,
+      doc,
+      operations,
     });
-
-    const promises = paths.map(async (path) => {
-      const firstPropValue = doc.paths[path];
-
-      const prompt = buildPrompt(JSON.stringify(firstPropValue));
-
-      const completion = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        messages: [
-          {
-            role: 'system',
-            content: 'You are a friendly AI assistant that only returns JSON',
-          },
-          {
-            role: 'user',
-            content: prompt,
-          },
-        ],
-      });
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return JSON.parse(completion.choices[0]?.message.content || '');
-    });
-
-    try {
-      const functionCalls = await Promise.all(promises);
-
-      const functionCallsString = JSON.stringify(functionCalls, null, 2);
-
-      const content = `export const functions = ${functionCallsString};`;
-
-      await writeFile('./generated/functions.ts', content, 'utf8');
-
-      console.log(
-        'File generated/functions.ts has been created with the function calls.'
-      );
-
-      await generateRunFunction(operations);
-
-      exec(
-        'npx eslint ./generated/functions.ts --fix',
-        (error, _stdout, stderr) => {
-          if (error) {
-            console.error(`Error running ESLint: ${error.message}`);
-            return;
-          }
-          if (stderr) {
-            console.error(`ESLint stderr: ${stderr}`);
-            return;
-          }
-
-          console.log('ESLint has been run on generated/functions.ts');
-        }
-      );
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
   };
 
   return (
--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -4,7 +4,19 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
+import { exec } from 'child_process';
+import { writeFile } from 'fs/promises';
+import OpenAI from 'openai';
+
 import { OpenAPIGenericSchema } from '../../types/openapi.js';
+
+import { generateRunFunction } from './ai.js';
+import { readEnvVars } from './env.js';
+import { buildPrompt } from './prompt.js';
+
+const openai = new OpenAI({
+  apiKey: readEnvVars().OPENAI_API_KEY,
+});
 
 export const resolveComponentReferences = (
   schema: OpenAPIGenericSchema
@@ -75,4 +87,135 @@ export const stripResponsesFromSchema = (
   });
 
   return schemaWithoutResponses;
+};
+
+export const getYamlContent = async (url: string) => {
+  const yamlContent = await fetch(url).then((res) => res.text());
+
+  return yamlContent;
+};
+
+export const generateSchema = (url: string) => {
+  const command = `npx openapi-typescript ${url} --o ./generated/api.ts`;
+
+  exec(command, (error, _stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      // Handle error here
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      // Handle stderr here
+      return;
+    }
+
+    console.log(
+      "File generated/api.ts has been created with the OpenAPI schema's types."
+    );
+  });
+};
+
+type OperationProps = {
+  paths: string[];
+  doc: OpenAPIGenericSchema;
+};
+
+export const getOperations = ({ paths, doc }: OperationProps) => {
+  const operations = paths.flatMap((path) => {
+    const pathValue = doc.paths[path];
+
+    if (!pathValue) {
+      throw new Error(`No value found for path ${path}`);
+    }
+
+    const methods = Object.keys(pathValue);
+
+    return methods.map((method) => {
+      const operation = pathValue[method];
+
+      if (!operation) {
+        throw new Error(`No value found for method ${method}`);
+      }
+
+      return {
+        path,
+        operationId: operation.operationId as string,
+        method,
+      };
+    });
+  });
+
+  return operations;
+};
+
+type GenerateFunctionCallsProps = OperationProps & {
+  operations: Array<{
+    path: string;
+    operationId: string;
+    method: string;
+  }>;
+};
+
+export const generateFunctionCalls = async ({
+  paths,
+  doc,
+  operations,
+}: GenerateFunctionCallsProps) => {
+  const promises = paths.map(async (path) => {
+    const firstPropValue = doc.paths[path];
+
+    const prompt = buildPrompt(JSON.stringify(firstPropValue));
+
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a friendly AI assistant that only returns JSON',
+        },
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return JSON.parse(completion.choices[0]?.message.content || '');
+  });
+
+  try {
+    const functionCalls = await Promise.all(promises);
+
+    const functionCallsString = JSON.stringify(functionCalls, null, 2);
+
+    const content = `export const functions = ${functionCallsString};`;
+
+    await writeFile('./generated/functions.ts', content, 'utf8');
+
+    console.log(
+      'File generated/functions.ts has been created with the function calls.'
+    );
+
+    await generateRunFunction(operations);
+
+    exec(
+      'npx eslint ./generated/functions.ts --fix',
+      (error, _stdout, stderr) => {
+        if (error) {
+          console.error(`Error running ESLint: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`ESLint stderr: ${stderr}`);
+          return;
+        }
+
+        console.log('ESLint has been run on generated/functions.ts');
+      }
+    );
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 };
